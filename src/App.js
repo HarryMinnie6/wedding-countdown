cc6c77737abc375565e001947813c776bd4214da
HarryMinnie6 <56819025+HarryMinnie6@users.noreply.github.com>
2021-05-20 15:11:45 +0200

    wedding count down completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import CountdownTimer from "./components/CountdownTimer/CountdownTimer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='top'>
+        <p className='top_save-the-date'>save the date</p>
+        <p className='top_for'>for the wedding celebration of</p>
+        <div className='top_name-holder'>
+          <div className='top_name-first'>
+            <p className='name'>HARRY</p>
+            <p className='surname'>Minnie</p>
+          </div>
+          <div>
+            <p className='and_sign'>and</p>
+          </div>
+          <div className='top_name-second'>
+            <p className='name'>CHANEL</p>
+            <p className='surname'>Vallun</p>
+          </div>
+        </div>
+      </div>
+
+      <div className='bottom'>
+        <CountdownTimer />
+        <div className='bottom__info'>
+          <p className='bottom__date'>September 30, 2022</p>
+          <p className='bottom__location'>Paarl, Cape Town</p>
+        </div>
+      </div>
     </div>
   );
 }
